8accee78dbf08f525faf182658e5c50fc1e07a34
RunningTalus <dave@davemay.org>
2015-07-11 01:37:07 -0600

    Increased the social media icon sizing.  Fighting Font Awesome and Twitter Bootstrap over Tooltip positioning.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 //window.onbeforeunload = function() {
 //    return "Two buttons will be below this message asking if user wants to stay on this page or leave.";
 //}
+
+$(function () {
+    $('[data-toggle=tooltip]').tooltip();
+        //$('[data-toggle=popover]').on('shown.bs.popover', function () {
+        //    $('.popover').css('top',parseInt($('.popover').css('top')) + 22 + 'px')
+        //});
+    //$("i.fa").popover({'trigger':'hover'});
+    //$('[data-toggle=popover]').popover();
+    //$('#githubPopover').popover();
+    //$('#codepenPopover').css('top','30px').popover();
+
+});
