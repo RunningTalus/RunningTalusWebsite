6f82681bc87cddf68d39efcbed0de530f922e236
RunningTalus <dave@davemay.org>
2015-07-13 17:39:25 -0600

    Removed JS for Bootstrap Tooltip.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
 //window.onbeforeunload = function() {
 //    return "Two buttons will be below this message asking if user wants to stay on this page or leave.";
 //}
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-});
